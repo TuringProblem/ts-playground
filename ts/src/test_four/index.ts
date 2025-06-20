0dca78d2d50ba311ae86e51152962552e39f2e10
Override <122590935+TuringProblem@users.noreply.github.com>
2025-06-19 20:10:01 -0400

    master: feat: added the recursive versioning, using functional and iterative approaches

---
+++ ts/src/test_four/index.ts
@@ -0,0 +1,27 @@
+function normalSumAll(xd: number[]): number {
+  let result = 0;
+
+  for (let i = 0; i < xd.length; i++) {
+    result += xd[i];
+  }
+  return result;
+}
+console.log(normalSumAll([1, 2, 3, 4, 5]));
+
+type SumAll = (xs: number[]) => number;
+
+const sumAll: SumAll = (xd) => {
+  // sum is the head of the list + the sum of the remaining [0] + 1
+  if (xd.length === 0) {
+    return 0;
+  }
+
+  const [head, ...rest] = xd;
+  return head + sumAll(rest);
+};
+
+console.log(` Now using recursing: [${sumAll([1, 2, 3, 4, 5])}]`);
+
+// more functional and readable version
+const sumAllTwo: SumAll = (xs) => (xs.length == 0 ? 0 : xs[0] + sumAllTwo(xs.slice(1)));
+console.log(` Now using recursing sumAllTwo : [${sumAllTwo([1, 2, 3, 4, 5])}]`);
